236165b87f5fdebc061257c6831df602c0d9ccfb
ahmedTonoy <forkan1510699@gmail.com>
2026-06-11 11:05:41 +0600

    chore: add basename to resolve 404 issue of Github page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -7,36 +7,41 @@ import Apps from "../pages/Apps/Apps";
 import AppDetails from "../pages/AppDetails/AppDetails";
 import InstalledApps from "../pages/InstalledApps/InstalledApps";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      Component: Root,
+      errorElement: <Page404></Page404>,
+      children: [
+        {
+          index: true,
+          loader: () => fetch("./appData.json"),
+          Component: Home,
+          HydrateFallback: LoadingSpinner,
+        },
+        {
+          path: "apps",
+          loader: () => fetch("./appData.json"),
+          Component: Apps,
+          HydrateFallback: LoadingSpinner,
+        },
+        {
+          path: "installation",
+          loader: () => fetch("./appData.json"),
+          Component: InstalledApps,
+          HydrateFallback: LoadingSpinner,
+        },
+        {
+          path: "appDetails/:id",
+          loader: () => fetch("./appData.json"),
+          Component: AppDetails,
+          HydrateFallback: LoadingSpinner,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    Component: Root,
-    errorElement: <Page404></Page404>,
-    children: [
-      {
-        index: true,
-        loader: () => fetch("/appData.json"),
-        Component: Home,
-        HydrateFallback: LoadingSpinner,
-      },
-      {
-        path: "apps",
-        loader: () => fetch("/appData.json"),
-        Component: Apps,
-        HydrateFallback: LoadingSpinner,
-      },
-      {
-        path: "installation",
-        loader: () => fetch("/appData.json"),
-        Component: InstalledApps,
-        HydrateFallback: LoadingSpinner,
-      },
-      {
-        path: "appDetails/:id",
-        loader: () => fetch("/appData.json"),
-        Component: AppDetails,
-        HydrateFallback: LoadingSpinner,
-      },
-    ],
+    basename: "/AppHub/",
   },
-]);
+);
